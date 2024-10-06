30d7dfdaee8e0b5aa33063ee9861aac568505cfb
Sila Mshelia <153488988+SilaMsheliaTech@users.noreply.github.com>
2024-10-06 06:49:56 +0100

    Add files via upload

--- Layoffs Data Cleaning.sql
+++ Layoffs Data Cleaning.sql
@@ -9,37 +9,37 @@
 
 
 SELECT *
-FROM layoffs;
+FROM world_layoffs.layoffs;
 
 
--- Creating a copy of layoffs table we will be working on
+-- Creating a Copy of Layoffs Table we will be Working on
 
 CREATE TABLE layoffs_staging
-LIKE layoffs;
+LIKE world_layoffs.layoffs;
 
 INSERT layoffs_staging
 SELECT *
-FROM layoffs;
+FROM world_layoffs.layoffs;
 
 
 -- 1. Checking and Removing Duplicates
 
 SELECT *,
 ROW_NUMBER() OVER(PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, "date") AS row_num
-FROM layoffs_staging;
+FROM world_layoffs.layoffs_staging;
 
 WITH duplicate_cte AS 
 (
 SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, "date", stage, country, funds_raised_millions) AS row_num
-FROM layoffs_staging
+FROM world_layoffs.layoffs_staging
 )
 SELECT *
 FROM duplicate_cte
 WHERE row_num > 1;
 
-CREATE TABLE `layoffs_staging2` (
+CREATE TABLE `world_layoffs`.`layoffs_staging2` (
   `company` text,
   `location` text,
   `industry` text,
@@ -52,54 +52,54 @@ CREATE TABLE `layoffs_staging2` (
   `row_num` int 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO layoffs_staging2
+INSERT INTO world_layoffs.layoffs_staging2
 SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, "date", stage, country, funds_raised_millions) AS row_num
-FROM layoffs_staging;
+FROM world_layoffs.layoffs_staging;
 
 SELECT *
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE row_num > 1;
 
 DELETE 
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE row_num > 1;
 
 
 -- 2. Standardizing Data
 
 SELECT company, TRIM(company)
-FROM layoffs_staging2;
+FROM world_layoffs.layoffs_staging2;
 
-UPDATE layoffs_staging2
+UPDATE world_layoffs.layoffs_staging2
 SET company = TRIM(company);
 
 SELECT DISTINCT industry
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 ORDER BY 1;
 
 SELECT *
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE industry LIKE "%Crypto%";
 
-UPDATE layoffs_staging2
+UPDATE world_layoffs.layoffs_staging2
 SET industry = "Crypto"
 WHERE industry LIKE "%Crypto%";
 
 SELECT DISTINCT country, TRIM(TRAILING "." FROM country)
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 ORDER BY 1;
 
-UPDATE layoffs_staging2
+UPDATE world_layoffs.layoffs_staging2
 SET country = TRIM(TRAILING "." FROM country)
 WHERE country LIKE "%United States%";
 
 SELECT `date`
 STR_TO_DATE(`date`, "%m/%d/%Y")
-FROM layoffs_staging2;
+FROM world_layoffs.layoffs_staging2;
 
-UPDATE layoffs_staging2
+UPDATE world_layoffs.layoffs_staging2
 SET `date` = STR_TO_DATE(`date`, "%m/%d/%Y");
 
 ALTER TABLE layoffs_staging2
@@ -109,26 +109,26 @@ MODIFY COLUMN `date` DATE;
 -- 3. Looking at Null Values and Blank Values
 
 SELECT *
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL;
 
-UPDATE layoffs_staging2
+UPDATE world_layoffs.layoffs_staging2
 SET industry = NULL
 WHERE industry = "";
 
 SELECT *
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE industry IS NULL
 OR industry = "";
 
 SELECT *
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE company = "Airbnb";
 
 SELECT t1.industry, t2.industry
-FROM layoffs_staging2 t1
-JOIN layoffs_staging2 t2
+FROM world_layoffs.layoffs_staging2 t1
+JOIN world_layoffs.layoffs_staging2 t2
 	ON t1.company = t2.company
     AND t1.location = t2.location 
 WHERE (t1.industry IS NULL OR t1.industry = "")
@@ -142,21 +142,21 @@ WHERE t1.industry IS NULL
 AND t2.industry IS NOT NULL;
 
 SELECT *
-FROM layoffs_staging2;
+FROM world_layoffs.layoffs_staging2;
 
 
 -- 4. Removing Columns or Rows
 
 SELECT *
-FROM layoffs_staging2;
+FROM world_layoffs.layoffs_staging2;
 
 SELECT *
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL;
 
 DELETE
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL;
 
